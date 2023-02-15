dbd1f71a2f59c92d043fa2d536ddb78763279927
Vishal Chand <sam.chand96@gmail.com>
2023-02-15 22:45:43 +0530

    Last push

--- app/src/main/java/com/example/foodify/homeActivity/MainActivity.kt
+++ app/src/main/java/com/example/foodify/homeActivity/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
